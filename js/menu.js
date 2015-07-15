0e36de27edfd456bea4a0aa7d42e298e79e5bb8a
JoseRaulAlfaro <joseraulalfaroguillen@gmail.com>
2015-07-14 21:15:55 -0600

    Headers - cambios
    Se incorporo el icono de la categoria en el header de las categorias.
    
    Se corrigieron algunos problemas que causaban q los iconos de categorias
    no se desplegaran correctamente

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,11 @@
 
-$(".logo").tapend(function(){
+menuOpen = function(){
 	$("#mainMenu").animate({"right" : "0px"});
 	$('#modal').fadeIn();
-});
+}
+
+
+$(".logo , .logoMenutxt").tapend(menuOpen);
 
 $("#modal").tapend(function(){
 	$("#mainMenu").animate({"right" : "-"+(window.innerWidth*0.8)+"px"});
--- js/transitions.js
+++ js/transitions.js
@@ -3,10 +3,12 @@
 $('.titles>tbody>tr>td>div').tap(function(){
 	$('#Categories').css('visibility','hidden');
 	var color = $(this).children().css('backgroundColor');
+	var icono = $(this).children('i').attr('class').split(" ");
+	icono.pop();
 	$('#mainTopbar').hide();
 	$('#categoryTopbar').css('visibility','visible');
 	$('#categoryTopbar').css('backgroundColor',color);
-	$('#categoryTopbar>i').html($(this).children().text());
+	$('#categoryTopbar>i').html("<span class='"+icono.join(" ")+"'></span> "+$(this).children().text());
 
 
 	});
