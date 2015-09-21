c27227d7bc7809ac4d042404782a78caa35a1135
Juan Andres Segreda Johanning <andres@sailorpro.com>
2015-09-21 00:20:45 -0600

    add first styles for groups

--- js/generics.js
+++ js/generics.js
@@ -1,3 +1,16 @@
 $.fn.hasAttr = function(name) {  
 	return this.attr(name) !== undefined;
 };
+
+//Star in Tap
+startTap = { X : 0 , Y : 0}
+$("*").tapstart(function(ev){
+	startTap.X = ev.pageX || ev.originalEvent.touches[0].pageX;
+	startTap.Y = ev.pageY || ev.originalEvent.touches[0].pageY;
+});
+
+function checkPress(ev){
+	var endX = ev.pageX || ev.originalEvent.changedTouches[0].pageX;
+	var endY = ev.pageY || ev.originalEvent.changedTouches[0].pageY;
+	return Math.abs(endX - startTap.X)  < 10 && Math.abs(endY - startTap.Y) < 10	
+}
--- js/group.js
+++ js/group.js
@@ -1,4 +1,23 @@
-$(".putimg").tapend(function(){
-	changePage("groupPage");
-	$("#categoryTopbar").css('backgroundImage',$(this).css('backgroundImage'));
+$(".putimg").tapend(function(ev){
+	console.dir(ev);
+	if(checkPress(ev)){
+		var color = $(this).find(".titlesLable").css("backgroundColor");
+		changePage("groupPage");
+		$("#categoryTopbar").css({
+			"background-image" : $(this).css('backgroundImage'),
+			"border-bottom" : "3px solid "+color
+		});
+		
+		$(".dotUnion").css({
+			"border-color" : color
+		})
+		
+		$(".gname").html($(this).find(".titlesLable td:eq(0)").html()).css("color",color)
+	}
+});
+
+
+var myScrollGroup = new IScroll('#wrapperGroup', {
+	probeType: 1, 
+	mouseWheel: false	,
 });
