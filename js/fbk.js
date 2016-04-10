e4cdfe727e07c77a7c2278ab18f493688294447a
Juan Andres Segreda Johanning <andres@sailorpro.com>
2016-04-09 18:55:06 -0600

    d

--- js/fbk.js
+++ js/fbk.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
       version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
     });     
     $('#loginbutton,#feedbutton').removeAttr('disabled');
-    FB.getLoginStatus(updateStatusCallback);
+    FB.getLoginStatus(fbLoginSuccess);
   });
 });
 
