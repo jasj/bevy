6c447b9abe051a2a10ded3bf781a63828457b09d
Juan Andres Segreda Johanning <andres@sailorpro.com>
2016-04-09 19:19:43 -0600

    d

--- js/fbk.js
+++ js/fbk.js
@@ -6,24 +6,9 @@ var fbLoginSuccess = function (userData) {
         alert("Could not get access token: " + err);
     });
 }
-
 //facebookConnectPlugin.browserInit(1522117891429463);
-
-$(document).ready(function() {
-  $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-	  alert("getScript");
-    FB.init({
-      appId: 1522117891429463
-      version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
-    });     
-    $('#loginbutton,#feedbutton').removeAttr('disabled');
-    FB.getLoginStatus(fbLoginSuccess);
-  });
-});
-
 try{
-facebookConnectPlugin.login([], fbLoginSuccess, function(){alert("samp")})
+	facebookConnectPlugin.login([], fbLoginSuccess, function(){alert("samp")})
 }catch(e){
 	alert(e);
 }
