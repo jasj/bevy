dd43099258413326d1cdee77abf8e9bd93985b9f
Juan Andres Segreda Johanning <andres@sailorpro.com>
2016-04-09 21:43:31 -0600

    d

--- js/fbk.js
+++ js/fbk.js
@@ -18,33 +18,18 @@ var fbLoginSuccess = function (userData) {
 		  xfbml      : true,
 		  version    : 'v2.5'
 		});
-		
-		FB.login(function(response) {
-    if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-});
+		   try{
+	facebookConnectPlugin.login([], fbLoginSuccess, function(){alert("samp")})
+}catch(e){
+	alert(e);
+}
+	
 	}catch(e){ alert(2);}
 	
   };
  
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+
    
    
-   try{
-	facebookConnectPlugin.login([], fbLoginSuccess, function(){alert("samp")})
-}catch(e){
-	alert(e);
-}
+
